8c6a08e586dbc44835ff623807e22997b768e624
Yosshiii43 <daisy440440@gmail.com>
2025-06-22 04:22:06 +0900

    SP-header

--- js/main.js
+++ js/main.js
@@ -142,6 +142,53 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 /*************************************************************************
+ * フェードイン
+ *************************************************************************/
+document.addEventListener('DOMContentLoaded', () => {
+  const targetElements = document.querySelectorAll('.js-fadeIn')
+  // 該当の要素が存在しなければ処理を終了
+  if (!targetElements) return
+
+  // ブラウザの内側の高さ
+  let windowHeight = window.innerHeight
+  // スクロール量
+  let lastScrollY = window.scrollY
+  // スクロールイベントの連続発火を防ぐためのフラグ
+  let isTicking = false
+
+  // 画面リサイズ時にwindowHeightを更新
+  window.addEventListener('resize', () => {
+    windowHeight = window.innerHeight
+  })
+
+  // 要素が表示された時にクラスを付与する関数
+  const inviewAnimation = () => {
+    targetElements.forEach((element) => {
+      const targetPosition = element.getBoundingClientRect().top + lastScrollY
+      const isInview = lastScrollY > targetPosition - windowHeight + 100
+      element.classList.toggle('is-inview', isInview)
+    })
+  }
+
+  // スクロールイベント発火時に実行する関数
+  const onScroll = () => {
+    // inviewAnimationを実行中は処理をスキップ
+    if (isTicking) return
+    lastScrollY = window.scrollY
+    isTicking = true
+    // 1フレーム待ってからinviewAnimationを実行
+    requestAnimationFrame(() => {
+      inviewAnimation()
+      isTicking = false
+    })
+  }
+
+  // スクロールイベントを登録
+  window.addEventListener('scroll', onScroll)
+})
+
+
+/*************************************************************************
  * アコーディオン
  *************************************************************************/
 document.querySelectorAll('.js-accordionDetails').forEach(details => {
