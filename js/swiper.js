d7b3b7abe0a3063242bc814b378998e341e8710d
Yosshiii43 <daisy440440@gmail.com>
2025-06-27 05:45:40 +0900

    fix:achievementsのCSS調整

--- js/swiper.js
+++ js/swiper.js
@@ -1,24 +1,18 @@
 const achievementsSwiper = new Swiper(".p-achievements__swiper", {
   loop: true,
-
-  speed: 4000,
-
+  speed: 12000,
   autoplay: {
     delay: 0,                 //止まらず流れる
     disableOnInteraction: false,
   },
-
-  /* 手動スワイプ不要なら触れないようにして滑らかに */
-  allowTouchMove: false,
-
+  
+  allowTouchMove: false,      // 手動スワイプ不要なら触れないようにして滑らかに 
   slidesPerView: 'auto',
-  spaceBetween: 63,            // ← SP の間隔
-
+  spaceBetween: 63,
   centeredSlides: true,
-
   breakpoints: {
     768: {
-      spaceBetween: 80         // ← tab の間隔
+      spaceBetween: 80
     }
   }
 });
